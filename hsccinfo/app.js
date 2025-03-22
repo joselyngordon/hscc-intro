fd4410916e476cf5ca7475075b63cba4d3242592
joselyngordon <joselyngordon1230@gmail.com>
2025-03-22 17:34:25 +0000

    updates

--- hsccinfo/app.js
+++ hsccinfo/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var bdpahistoryRouter = require('./routes/bdphistory')
 var app = express();
 
 // view engine setup
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/bdpahistory',bdpahistoryRouter);
+// app.use()
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
